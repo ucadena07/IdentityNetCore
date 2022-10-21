b6059457a894aca5329feb8079f91a6cf44ab85e
ucadena07 <ucadena07@gmail.com>
2022-10-21 06:34:35 -0500

    db setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAndSecurity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+    }
+}
