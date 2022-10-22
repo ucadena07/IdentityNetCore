9c18c56154e4212988c1f18194dad5c6661dd864
ucadena07 <ucadena07@gmail.com>
2022-10-22 07:25:43 -0500

    Creating user sign-up part 2

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,19 +1,42 @@
 ﻿using IdentityAndSecurity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityAndSecurity.Controllers
 {
     public class IdentityController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public async Task<IActionResult> Signup()
         {
             var model = new SignupDto();
-            return View(model);
+            return await Task.FromResult(View(model));
         }
 
         [HttpPost]
         public async Task<IActionResult> Signup(SignupDto model)
         {
+            if (ModelState.IsValid)
+            {
+                if((await _userManager.FindByEmailAsync(model.Email) == null))
+                {
+                    var user = new IdentityUser { Email = model.Email, UserName = model.Email };
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Signin");
+                    }
+                    ModelState.AddModelError("Signup",String.Join(" ", result.Errors.Select(it => it.Description)));
+                    return View(model);
+                }
+            }
             return View(model);
         }
         public async Task<IActionResult> Signin()
